89e34e632dce43ce2537bedf3fd5b4695d94f69a
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-23 00:43:55 +0600

    Add comparison operators practice

---
+++ 01-basics/03_comparison_operators.py
@@ -0,0 +1,22 @@
+# Comparison operators
+
+a = 10
+b = 5
+
+# Equal to
+print("Equal:", a == b)
+
+# Not equal to
+print("Not equal:", a != b)
+
+# Greater than
+print("Greater than:", a > b)
+
+# Less than
+print("Less than:", a < b)
+
+# Greater than or equal to
+print("Greater than or equal:", a >= b)
+
+# Less than or equal to
+print("Less than or equal:", a <= b)
