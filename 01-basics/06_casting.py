325cdab7c6f8c42d6c2838f850c2addb6cd3a8e2
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-26 22:10:06 +0600

    06_casting.py

---
+++ 01-basics/06_casting.py
@@ -0,0 +1,21 @@
+# Python Casting
+
+# Integer to float
+x = 10
+print(float(x))
+
+# Float to integer
+y = 5.7
+print(int(y))
+
+# Integer to string
+age = 21
+print(str(age))
+
+# String to integer
+number = "25"
+print(int(number))
+
+# String to float
+price = "19.99"
+print(float(price))
