e00aebb8a6adeded33788d39607fbff0e5b1af53
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-26 22:11:27 +0600

    Add strings practice

---
+++ 01-basics/07_strings.py
@@ -0,0 +1,41 @@
+# Python Strings
+
+# Creating a string
+name = "Arnob"
+
+print(name)
+
+# Strings can use single or double quotes
+first_name = 'Arnob'
+last_name = "Fardin"
+
+print(first_name)
+print(last_name)
+
+# String with a variable
+age = 21
+message = "My name is Arnob and I am 21"
+
+print(message)
+
+# Multiline string
+text = """Python is easy to learn.
+Python is powerful.
+Python is popular."""
+
+print(text)
+
+# Access a character using an index
+word = "Python"
+
+print(word[0])
+print(word[1])
+
+# Get the length of a string
+print(len(word))
+
+# Check if something is inside a string
+print("Py" in word)
+
+# Check if something is NOT inside a string
+print("Java" not in word)
