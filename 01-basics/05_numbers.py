eb49b454af7784c56a807c7c5f0248c854717a2b
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-26 22:08:46 +0600

    Add numbers practice

---
+++ 01-basics/05_numbers.py
@@ -0,0 +1,26 @@
+# Python Numbers
+
+# Integer
+x = 10
+
+# Float
+y = 3.14
+
+# Complex
+z = 2 + 3j
+
+print(x)
+print(y)
+print(z)
+
+# Check data types
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Scientific notation
+a = 35e3
+b = 12E4
+
+print(a)
+print(b)
