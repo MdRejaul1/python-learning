f19333239922d4beadc501eab5a8e75dd1ad3c7c
Md Rejaul Karim <mdrejaulkarimroman1@gmail.com>
2026-09-26 22:06:11 +0600

    Add data types practice

---
+++ 01-basics/04_data_types.py
@@ -0,0 +1,19 @@
+# Python Data Types
+
+# String
+name = "Arnob"
+
+# Integer
+age = 21
+
+# Float
+height = 5.8
+
+# Boolean
+is_student = True
+
+# Check data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
